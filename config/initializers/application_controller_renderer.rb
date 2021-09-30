df18c85b131e84883ba4aed0addd418b4e0e89d5
koedashintarou <ec2-user@ip-172-31-55-136.ec2.internal>
2021-09-30 08:51:40 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '5b2a4d6d1720435aef6d57a928d00bfbf9fdaaf7e6d41f0f98edba41c11d18ae5674d6f4392503c51986600594dab1c5f04b1a37120c2550b2fad4c71107ec41'
